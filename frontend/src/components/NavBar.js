322bc078282afd10544c90762ca9c7cc6cdcc0a1
solenwa <solen.wanono@gmail.com>
2024-10-24 14:24:57 +0200

    Initialize repo

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavBar() {
+  return (
+    <Navbar expand="lg" className="me-auto w-100 justify-content-end">
+      <Container>
+        <Navbar.Brand href="#home">The Cellar</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#macave">Ma Cave</Nav.Link>
+            <Nav.Link href="#link">Mes Dégustations</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
