a6edbf4a8ec3a9a81eaa43828ba76f67b19d4b3a
solenwa <solen.wanono@gmail.com>
2024-10-25 14:51:16 +0200

    Backend logic added for user signin

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,70 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import expressAsyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
+import { isAuth, generateToken } from '../utils.js';
+
+const userRouter = express.Router();
+
+userRouter.get(
+  '/',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.send(users);
+  })
+);
+
+userRouter.get(
+  '/:id',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(404).send({ message: 'User Not Found' });
+    }
+  })
+);
+
+userRouter.post(
+  '/login',
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          token: generateToken(user),
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: 'Invalid email or password' });
+  })
+);
+
+userRouter.post(
+  '/signup',
+  expressAsyncHandler(async (req, res) => {
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password),
+    });
+    const user = await newUser.save();
+    res.send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user),
+    });
+  })
+);
+
+export default userRouter;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config_db.js';
-import productRoute from './routes/productRoutes.js';
+import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -10,17 +11,18 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-/* app.use((err, req, res, next) => {
-    res.status(500).send({ message: err.message });
-  });
-  */
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
 
-app.use('/api/products', productRoute);
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 // Health check path
 app.get('/health', (req, res) => {
